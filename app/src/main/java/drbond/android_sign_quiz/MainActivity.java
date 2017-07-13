984d078dc7d43ab4fe1fb33dd7845233d302e374
Ryan Bond <drbond@gmail.com>
2017-07-13 15:55:34 -0500

    added button clicks and answer output.

--- app/src/main/java/drbond/android_sign_quiz/MainActivity.java
+++ app/src/main/java/drbond/android_sign_quiz/MainActivity.java
@@ -2,11 +2,15 @@ package drbond.android_sign_quiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Button;
+
 
 
 
 public class MainActivity extends AppCompatActivity {
+
     private TextView answerText;
 
     @Override
@@ -14,9 +18,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Create Buttons
+        Button button1;
+        Button button2;
+        Button button3;
+        Button button4;
 
 
-    }
+        // Map Buttons to IDs
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+
+        //Map TextView
+        answerText = (TextView) findViewById(R.id.answerText);
+
+        // Create Button Click Listeners
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                answerText.setText("WRONG");
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                answerText.setText("WRONG");
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                answerText.setText("Right");
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                answerText.setText("WRONG");
+            }
+        });
 
 
-}
+    }}
