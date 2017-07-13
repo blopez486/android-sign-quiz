50b727fc8685f73f4902e42ed3fbb6a9dea53cc1
Ryan Bond <drbond@gmail.com>
2017-07-13 15:11:44 -0500

    moved all images to drawable, and created static values in the buttons for testing.

--- app/src/main/java/drbond/android_sign_quiz/MainActivity.java
+++ app/src/main/java/drbond/android_sign_quiz/MainActivity.java
@@ -2,12 +2,21 @@ package drbond.android_sign_quiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+
 
 public class MainActivity extends AppCompatActivity {
+    private TextView answerText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
+
+
 }
